b07e76c4de6b05cf87e26aa313a4b2123d2e0b42
manhtieneaut <20200892@eaut.edu.vn>
2023-02-08 19:08:36 +0700

    Bài thực hành 3

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import react from "react";
+import "./App.css";
+
+export default function App() {
+
+  return (
+    <div className="container">
+      <h1>Login</h1>
+      <form>
+        <p>nhập email:</p>
+        <input
+          name="email"
+          type="text"
+        />
+        <p>nhập password:</p>
+        <input
+          name="password"
+          type="password"
+        />
+        <br />
+        <label>
+          <input
+            name="isRead"
+            type="checkbox" />I read and accept the privacy policy:
+        </label>
+
+        <p>bấm submit form</p>
+        <button>submit nè</button>
+      </form>
+    </div>
+  );
+}
